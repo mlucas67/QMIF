4f14e2395e151e4f7044067df5eba0a4654e3566
Marshall Lucas <mlucas67@gmail.com>
2013-03-22 16:09:24 -0500

    Beginning work on parser.
    Have it figured out now.

---
+++ CLASSLIB/Verbs.h
@@ -0,0 +1,11 @@
+$ifndef Verbs_h
+
+    $define Verbs_h 1
+    
+    Verbs = ''; Actions = ''
+    Verbs<1> = "'take '0Xý'get '0Xý'carry '0Xý'hold '0X"; Actions<1> = "*TAKE"
+    Verbs<2> = "'*TAKE out'ý'*TAKE off'ý'*TAKE up'"; Actions<2> = "Exit"
+    Verbs<3> = "'*TAKE in '0Xý'*TAKE into '0Xý'*TAKE on '0X"; Actions<3> = "Enterý2"
+    Verbs<4> = "'*TAKE off '0X"; Actions<4> = 
+
+$endif
--- CLASSLIB/constants.h
+++ CLASSLIB/constants.h
@@ -8,6 +8,7 @@ $ifndef constants_h
     $define TYPE_ROOM 1
     $define TYPE_DOOR 2
     $define TYPE_THING 3
+    $define TYPE_REGION 4
     
     $define nothing object("NOTHING")
 
